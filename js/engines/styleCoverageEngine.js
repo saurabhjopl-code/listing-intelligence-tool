509fed0fbbc0ca9e39817c4773a56b7838419a7a
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:17:31 +0530

    Update styleCoverageEngine.js

--- js/engines/styleCoverageEngine.js
+++ js/engines/styleCoverageEngine.js
@@ -1,1 +1,33 @@
+export function computeStyleCoverage(skuStatus){
 
+const map = {};
+
+skuStatus.forEach(row=>{
+
+if(!map[row.styleid]){
+
+map[row.styleid]={
+
+styleid:row.styleid,
+
+total:0,
+
+live:0
+
+};
+
+}
+
+map[row.styleid].total++;
+
+if(row.status==="LIVE"){
+
+map[row.styleid].live++;
+
+}
+
+});
+
+return Object.values(map);
+
+}
