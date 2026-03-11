f2cf054c332997694121e44c623a773da53b1f0c
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:17:55 +0530

    Update table.js

--- js/components/table.js
+++ js/components/table.js
@@ -1,1 +1,35 @@
+export function createTable(columns,data){
 
+let html = "<table class='table'>";
+
+html += "<thead><tr>";
+
+columns.forEach(col=>{
+
+html += `<th>${col}</th>`;
+
+});
+
+html += "</tr></thead>";
+
+html += "<tbody>";
+
+data.forEach(row=>{
+
+html += "<tr>";
+
+columns.forEach(col=>{
+
+html += `<td>${row[col] || ""}</td>`;
+
+});
+
+html += "</tr>";
+
+});
+
+html += "</tbody></table>";
+
+return html;
+
+}
