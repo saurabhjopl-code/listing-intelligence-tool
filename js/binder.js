1407ef76a68ab0bfbdb88d20c7ac4d6321f19702
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:16:15 +0530

    Update binder.js

--- js/binder.js
+++ js/binder.js
@@ -1,1 +1,30 @@
+import { loadSheets } from "./core/sheetLoader.js";
 
+import { buildCatalog } from "./engines/masterCatalogEngine.js";
+
+import { buildListingMap } from "./engines/listingPresenceEngine.js";
+
+import { computeSkuStatus } from "./engines/skuStatusEngine.js";
+
+import { computeStyleCoverage } from "./engines/styleCoverageEngine.js";
+
+import { renderSummary } from "./renderers/summaryRenderer.js";
+
+
+async function init(){
+
+const sheets = await loadSheets();
+
+const catalog = buildCatalog(sheets);
+
+const listings = buildListingMap(sheets);
+
+const skuStatus = computeSkuStatus(catalog,listings);
+
+const styleCoverage = computeStyleCoverage(skuStatus);
+
+renderSummary(styleCoverage);
+
+}
+
+init();
