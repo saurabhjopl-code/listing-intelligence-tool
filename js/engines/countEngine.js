b0e3efbead98e0ad14e9882d0bdac6d9962692bc
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 15:24:59 +0530

    Create countEngine.js

---
+++ js/engines/countEngine.js
@@ -0,0 +1,22 @@
+export function buildCount(master,data){
+
+const map={};
+
+data.forEach(r=>{
+
+if(!map[r.uniware_sku]) map[r.uniware_sku]=0;
+
+map[r.uniware_sku]++;
+
+});
+
+return master.map(r=>({
+
+uniware_sku:r.uniware_sku,
+styleid:r.styleid,
+category:r.category,
+count:map[r.uniware_sku]||0
+
+}));
+
+}
