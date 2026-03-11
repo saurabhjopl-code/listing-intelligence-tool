575445c085db8a2161f0911727c1fbcefdcedc5f
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 16:40:39 +0530

    Update countEngine.js

--- js/engines/countEngine.js
+++ js/engines/countEngine.js
@@ -1,24 +1,24 @@
 export function buildCount(master,data){
 
-const map={};
+const map={}
 
 data.forEach(r=>{
 
 if(!map[r.uniware_sku]){
-map[r.uniware_sku]=new Set();
+map[r.uniware_sku]=0
 }
 
-map[r.uniware_sku].add(r.mp_sku);
+map[r.uniware_sku]++
 
-});
+})
 
 return master.map(r=>({
 
 uniware_sku:r.uniware_sku,
 styleid:r.styleid,
 category:r.category,
-count: map[r.uniware_sku] ? map[r.uniware_sku].size : 0
+count: map[r.uniware_sku] || 0
 
-}));
+}))
 
 }
