768923df3647daf240e35e7e94d90b39828cbca4
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 15:23:48 +0530

    Update sheetLoader.js

--- js/core/sheetLoader.js
+++ js/core/sheetLoader.js
@@ -1,46 +1,28 @@
-const SHEETS = {
+const SHEETS={
 
-data: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=765260582&single=true&output=csv",
+data:"https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=765260582&single=true&output=csv",
 
-channel_name: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=1645772386&single=true&output=csv",
+channel:"https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=1645772386&single=true&output=csv",
 
-master_listings: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=69497712&single=true&output=csv",
-
-size_count: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=1410030111&single=true&output=csv",
-
-uniware_stock: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=2106875462&single=true&output=csv"
+master:"https://docs.google.com/spreadsheets/d/e/2PACX-1vT-JkfNtpddZXlSelTeFcrXsS4lZaZQdZu2ZdApqtaB7PDGTfeBnH_zhGQB3yQuqvvBRotFfD-q0HV7/pub?gid=69497712&single=true&output=csv"
 
 };
 
+async function loadCSV(url){
 
-async function fetchCSV(url){
+const res=await fetch(url);
+const text=await res.text();
 
-const res = await fetch(url);
+const lines=text.trim().split("\n");
 
-const text = await res.text();
+const headers=lines[0].split(",");
 
-return parseCSV(text);
+return lines.slice(1).map(l=>{
 
-}
+const v=l.split(",");
+const obj={};
 
-
-function parseCSV(text){
-
-const lines = text.trim().split("\n");
-
-const headers = lines[0].split(",");
-
-return lines.slice(1).map(row=>{
-
-const values = row.split(",");
-
-const obj = {};
-
-headers.forEach((h,i)=>{
-
-obj[h.trim()] = values[i] ? values[i].trim() : "";
-
-});
+headers.forEach((h,i)=>obj[h.trim()]=v[i]);
 
 return obj;
 
@@ -48,19 +30,14 @@ return obj;
 
 }
 
-
 export async function loadSheets(){
 
-const sheets = {};
+return{
 
-for(const key in SHEETS){
+data:await loadCSV(SHEETS.data),
+channel:await loadCSV(SHEETS.channel),
+master:await loadCSV(SHEETS.master)
 
-sheets[key] = await fetchCSV(SHEETS[key]);
-
-}
-
-console.log("Sheets Loaded", sheets);
-
-return sheets;
+};
 
 }
